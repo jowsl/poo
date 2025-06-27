440c5a28d5ff9791e1b02c97209a95f3ae214ea0
Guenrick <147289316+Guenrick@users.noreply.github.com>
2025-06-27 09:38:51 -0300

    Create GerenciadorVoos.h

---
+++ GerenciadorVoos.h
@@ -0,0 +1,33 @@
+#ifndef GERENCIADORVOOS_H
+#define GERENCIADORVOOS_H
+
+#include "Aeronave.h"
+#include "Piloto.h"
+#include "Passageiro.h"
+#include "Voo.h"
+#include <vector>
+
+using namespace std;
+
+class GerenciadorVoos {
+private:
+    vector<Aeronave*> aeronaves;
+    vector<Piloto*> pilotos;
+    vector<Passageiro*> passageiros;
+    vector<Voo*> voos;
+
+public:
+    GerenciadorVoos();
+    ~GerenciadorVoos(); // Destrutor
+
+    // Em GerenciadorVoos.h, dentro de public:
+    void cadastrarAeronave();
+    void cadastrarPiloto();
+    void cadastrarPassageiro();
+    void criarVoo();
+    void embarcarPassageiroVoo();
+    void listarVoos() const;
+    void listarPassageirosDeVoo() const;
+};
+
+#endif 
